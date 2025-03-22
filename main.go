5dc09d59089f947d7728248e2432ed1930de831d
jonathanhunt <jhun728@wgu.edu>
2025-03-22 11:34:24 -0500

    Made a reader and scanner for user input

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	// If 
+	r := os.Stdin
+	s := bufio.NewScanner(r)
+	// Runs a Loop For Commands Until Prompted To Exit
+	for {
+		fmt.Print("DegreeProgressTracker > ")
+		if ok := s.Scan(); !ok {
+			break
+		}
+	}
 }
